af18a1439fbe4e788b8c38c515b448025c681ed8
jiaeyan <svenyan234@gmail.com>
2018-11-04 03:42:09 -0800

    added shell script

--- collect_cards.sh
+++ collect_cards.sh
@@ -4,4 +4,4 @@ echo "Converting cdb to sql file..."
 sqlite3 $1/cards.cdb .dump > $1/cards.sql
 
 echo "Successfully converted cdb to sql file, now collecting card date..."
-python yugioh/cards/main.py $1
+python main.py $1
