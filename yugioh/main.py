7b9368c3049c0831b9a0f2b7bbc22b4cec09e322
jiaeyan <svenyan234@gmail.com>
2018-11-04 02:04:44 -0800

    added card_data folder

--- yugioh/main.py
+++ yugioh/main.py
@@ -3,7 +3,7 @@ from yugioh.cards.card_collector import CardCollector
 
 def main():
     yugioh = CardCollector()
-    yugioh.collect_cards('resources/card_data.sql', 'resources/strings.conf')
+    yugioh.collect_cards('resources/cards.sql', 'resources/strings.conf')
     yugioh.write_data()
 
 
